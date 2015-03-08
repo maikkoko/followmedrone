c25b450beee82ef1892175b55036e5a992980b6b
maikkoko <maikko.aleman@gmail.com>
2015-03-08 17:26:19 +0800

    Update receiver.ino
    Changed algo for bearing difference. Output is binary on pins 2-10 (Max: 180 Decimal)

--- receiver.ino
+++ receiver.ino
@@ -12,6 +12,18 @@ const float Pi = 3.14159;
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(12345);
 
+const int D0 = 2;
+const int D1 = 3;
+const int D2 = 4;
+const int D3 = 5;
+const int D4 = 6;
+const int D5 = 7;
+const int D6 = 8;
+const int D7 = 9;
+const int NEG = 10;
+
+int binary[8];
+int negval;
 float rcvHeading, transHeading;
 
 void setup() {
@@ -22,6 +34,8 @@ void setup() {
     }
     if (!driver.init())
         Serial.println("init failed");
+        
+    setupPins(); 
 }
 
 void loop() {
@@ -29,13 +43,21 @@ void loop() {
     transHeading = receiveCompassReading();
        
     if(transHeading >= 0) {
+        
+        //for debugging purposes
         Serial.print("rcv: ");Serial.println(rcvHeading);
         Serial.print("trans: ");Serial.println(transHeading);
         float diff = getBearingDiff(rcvHeading, transHeading);
         Serial.print("diff: ");Serial.println(diff);
+        
+        delay(200);
+        // -end
+        
+        convertToBinary((int)diff);
+        setBinaryPins();
     }
     
-    delay(1000);
+    delay(300);
 }
 
 float getCompassReading(void) {
@@ -76,9 +98,69 @@ float receiveCompassReading(void) {
 }
 
 float getBearingDiff(float rcv, float trans){
- 
-     if(abs(rcv - trans) >= 180)
-       return ((rcv - trans) + 360);
-     else 
-       return (-1) * (trans - rcv);    
+    if (trans > rcv) {
+        if ((trans - rcv) >= 180)
+            return (-1)*((rcv - trans) + 360);
+        else
+            return (-1)*(trans - rcv);
+    }
+    else {
+        if ((rcv - trans) >= 180)
+            return ((trans - rcv) + 360);
+        else
+            return (rcv - trans);
+    }    
+}
+
+void convertToBinary(int num) {
+    if (num < 0) {
+        negval = 1;
+        num = abs(num);
+    } 
+    else
+        negval = 0;
+        
+    convertNumToBinary(num);
+    
+    Serial.print(negval);
+    for (int i=7; i >= 0; i--) {
+        Serial.print(binary[i]);
+    }    
+    Serial.println();
+}
+
+void convertNumToBinary(int number) {
+    for(int i = 0; i < 8; i++) {
+        if ((number%2) == 1) {
+            binary[i] = 1;
+        }
+        else
+            binary[i] = 0;
+        
+        number = number/2;              
+    }
+}
+
+void setBinaryPins(){
+    digitalWrite(D0, binary[0]);
+    digitalWrite(D1, binary[1]);
+    digitalWrite(D2, binary[2]);
+    digitalWrite(D3, binary[3]);
+    digitalWrite(D4, binary[4]);
+    digitalWrite(D5, binary[5]);
+    digitalWrite(D6, binary[6]);
+    digitalWrite(D7, binary[7]);
+    digitalWrite(NEG, negval);
+}
+
+void setupPins(){
+    pinMode(D0, OUTPUT);
+    pinMode(D1, OUTPUT);
+    pinMode(D2, OUTPUT);
+    pinMode(D3, OUTPUT);
+    pinMode(D4, OUTPUT);
+    pinMode(D5, OUTPUT);
+    pinMode(D6, OUTPUT);
+    pinMode(D7, OUTPUT);
+    pinMode(NEG, OUTPUT); 
 }
