07f93a203acbd2771545ae14cdda9fa8db8f6120
maikkoko <maikko.aleman@gmail.com>
2015-04-27 20:11:54 +0800

    Updated to use RFM22B

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,58 +1,63 @@
-//Maikko A. Aleman 2010-49834
+// Maikko Aleman
 
-#include <RH_ASK.h>
-#include <SPI.h> 
+#include <SPI.h>
+#include <RH_RF22.h>
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303_U.h>
 
-RH_ASK driver;
-const float Pi = 3.14159;
+/* Singleton instance of the radio driver */
+RH_RF22 rf22;
 
-/* Assign a unique ID to this sensor at the same time */
+/* Unique ID to this sensor at the same time */
 Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(12345);
 
+const float Pi = 3.14159;
 float compassReading;
 
-void setup() {
-    Serial.begin(9600);	  
-    if(!mag.begin()) {
-        Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
-        while(1);
-    }
-    if (!driver.init())
-        Serial.println("init failed");
+void setup() {  
+  Serial.begin(9600);
+  if(!mag.begin()) {
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+  if (!rf22.init()) {
+    Serial.println("No RFM22B detected... Check your wiring!");
+    while(1);
+  }
 }
 
+
 void loop() {
-    compassReading = getCompassReading();
-    sendCompassReading(compassReading);
-    
-    Serial.print("sent: ");Serial.println(compassReading); //for debugging
+  compassReading = getCompassReading();
+  sendCompassReading(compassReading);
+  
+  
 }
 
 float getCompassReading(void) {
-    /* Get a new sensor event */
-    sensors_event_t event;
-    mag.getEvent(&event);
-
-    // Calculate the angle of the vector y,x
-    float heading = (atan2(event.magnetic.y,event.magnetic.x) * 180) / Pi;
-    // Normalize to 0-360
-    if (heading < 0) {
-        heading = 360 + heading;
-    }
-    
-    return heading;
+  /* Get a new sensor event */
+  sensors_event_t event;
+  mag.getEvent(&event);
+  // Calculate the angle of the vector y,x
+  float heading = (atan2(event.magnetic.y,event.magnetic.x) * 180) / Pi;
+  // Normalize to 0-360
+  if (heading < 0) {
+    heading = 360 + heading;
+  }
+  Serial.print(compassReading); //for debugging
+  return heading;
 }
 
 void sendCompassReading(float heading) {
-    char msg[6];
-    dtostrf(heading, 3, 2, msg);
-
-    delay(50);
-
-    driver.send((uint8_t *)msg, strlen(msg));
-    driver.waitPacketSent();
-    delay(100);
+  char msg[6];
+  dtostrf(heading, 3, 2, msg);
+  delay(50);
+  
+  rf22.send((uint8_t *)msg, strlen(msg));
+  rf22.waitPacketSent();
+  delay(100);
+  
+  Serial.println(" Sent!");
 }
